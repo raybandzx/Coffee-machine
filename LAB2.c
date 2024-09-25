dd4616b96db2ad2443a27ca3d67c7bbf8ae4c08f
rayakulsoomrafeek <merayakulsoom@gmail.com>
2024-09-25 16:24:29 +0400

    Add files via upload

---
+++ LAB2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define esp_beans 8
+#define capp_beans 8
+#define mocha_beans 8
+
+#define esp_water 30
+#define capp_water 30
+#define mocha_water 39
+
+#define esp_milk 0
+#define capp_milk 70
+#define mocha_milk 160
+
+#define mocha_chocolate 30
+
+#define esp_price 3.5
+#define capp_price 4.5
+#define mocha_price 5.5
